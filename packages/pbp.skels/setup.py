05a46605f3becab475f2417dbaeb05d75e80319c
tarek <tarek@MacZiade>
2008-07-10 14:25:19 +0200

    raised the new version

--- packages/pbp.skels/setup.py
+++ packages/pbp.skels/setup.py
@@ -20,7 +20,7 @@ This module contains the tool of pbp.skels
 import os
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.1.1'
 
 README = os.path.join(os.path.dirname(__file__),
                       'pbp', 'skels', 'docs', 'README.txt')
