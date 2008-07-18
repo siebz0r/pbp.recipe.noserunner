973be7432353a1d43d79f280ba3eb5889c8fee95
tarek <tarek@MacZiade.ingenidev>
2008-07-18 17:37:31 +0200

    fixed metadata

--- packages/release.sh
+++ packages/release.sh
@@ -1,36 +1,44 @@
 cd atomisator.db
 python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
 rm -rf *egg-info build dist
+cd ..
 
 cd atomisator.feed
 python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
 rm -rf *egg-info build dist    
+cd ..
 
 cd atomisator.main
 python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
 rm -rf *egg-info build dist    
+cd ..
 
 cd atomisator.parser
 python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
 rm -rf *egg-info build dist    
+cd ..
 
 cd pbp.buildbotenv
 python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
 rm -rf *egg-info build dist    
+cd ..
 
 cd pbp.recipe.noserunner
-python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
+python setup.py egg_info -RDb "" mregister sdist mupload -r "tarek"
 rm -rf *egg-info build dist    
+cd ..
 
 cd pbp.recipe.trac
-python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
+python setup.py egg_info -RDb "" mregister sdist mupload -r "tarek"
 rm -rf *egg-info build dist    
+cd ..
 
 cd pbp.scripts
-python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
+python setup.py egg_info -RDb "" mregister sdist mupload -r "tarek"
 rm -rf *egg-info build dist    
+cd ..
 
 cd pbp.skels
-python setup.py egg_info -RDb "" mregister sdist mupload -r "book"
+python setup.py egg_info -RDb "" mregister sdist mupload -r "tarek"
 rm -rf *egg-info build dist    
-
+cd ..
