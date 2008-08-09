74b8380500dc6a6ea8bce96f5c9af7628c72695e
tarek <tarek@MacZiade>
2008-08-10 01:33:58 +0200

    fixed unicode behavior

--- packages/atomisator.feed/setup.py
+++ packages/atomisator.feed/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2.2'
+version = '0.2.3'
 long_description = open("README.txt").read()
 classifiers=[
         "Programming Language :: Python",
