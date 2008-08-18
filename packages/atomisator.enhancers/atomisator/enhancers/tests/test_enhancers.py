97996a6b0c83f28668ddad9543b4a5a6ead710cc
tarek <tarek@localhost>
2008-08-18 06:58:05 +0200

    finished digg test

--- packages/atomisator.enhancers/atomisator/enhancers/tests/test_enhancers.py
+++ packages/atomisator.enhancers/atomisator/enhancers/tests/test_enhancers.py
@@ -1,8 +1,35 @@
 from atomisator.enhancers import DiggComments
+from atomisator.enhancers.digg import Digg
+
 from nose.tools import *
 
-def test_digg():
+def setup():
+    class Story:
+        id = u'9878756'
+
+    def one(*args, **kw):
+        return [Story()]
     
+    global old, old2
+    old = Digg.getStories
+    Digg.getStories = one
+
+    class Comment:
+        content = 'good comment'
+
+    def one_comment(*args, **kw):
+        return [Comment()]
+    
+    old2 = Digg.getStoriesComments
+    Digg.getStoriesComments = one_comment
+
+def teardown():
+    Digg.getStories = old
+    Digg.getStoriesComments = old2
+
+@with_setup(setup, teardown)
+def test_digg():
+
     class E:
         url = 'http://mail.python.org/pipermail/python-dev/2006-December/070323.html'
         summary = 'xxx'
