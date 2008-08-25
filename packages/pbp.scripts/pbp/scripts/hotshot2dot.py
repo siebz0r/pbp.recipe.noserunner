13eae0ef7a1a63b8dbba4e6d89fc665134be526d
tarek <tarek@localhost>
2008-08-25 06:52:27 +0200

    fixed argv

--- packages/pbp.scripts/pbp/scripts/hotshot2dot.py
+++ packages/pbp.scripts/pbp/scripts/hotshot2dot.py
@@ -1,23 +1,24 @@
 import sys
-from pbp.scripts.hotshotmain import run
+import hotshot.stats
 from pbp.scripts.gprof2dot import run_script
 
 def main():
-    if (sys.argv) != 2:
+    if len(sys.argv) != 2:
         print 'Usage: %s HOTSHOT_FILE' % sys.argv[0]
-
+        sys.exit(0)
     filename = sys.argv[1]
+    s = hotshot.stats.load(filename)
     pstats = filename + '.pstats'
-    ouput = filename + '.png' 
-
-    # building the pstats file  
-    run(filename, pstats)
     
-    # calling graph2dot
-    sys.argv = sys.argv[0] + ['-f', 'pstats', pstats, 
-                              '|', 'dot', '-Tpng', '-o', output]
-
-    run_script()
+    # building the pstats file  
+    s.dump_stats(pstats)
+   
+    try:
+        # calling graph2dot
+        sys.argv = [sys.argv[0], '-f', 'pstats', pstats] 
+        run_script()
+    finally:
+        os.remove(pstats)
 
 if __name__ == '__main__':
     main()
