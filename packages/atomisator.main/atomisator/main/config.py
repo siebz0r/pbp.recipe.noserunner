be35ddcdd2dc139acb59d5ea3a7e4b12220133c4
tarek <tarek@localhost>
2008-09-22 08:11:11 +0200

    better configuration parsing

--- packages/atomisator.main/atomisator/main/config.py
+++ packages/atomisator.main/atomisator/main/config.py
@@ -2,6 +2,7 @@ import os
 import sys
 from os.path import join
 from ConfigParser import ConfigParser
+import re
 
 DEFAULT_CONFIG_FILE = 'atomisator.cfg'
 
@@ -17,19 +18,56 @@ class AtomisatorConfig(object):
         self._file = cfg
         self._parser.read([cfg])
 
+    #
+    # basic APIs
+    #
+    def _get_simple_field(self, field, default=None):
+        if not self._parser.has_option('atomisator', field):
+            return default
+        return self._parser.get('atomisator', field).strip()
+    
+    def _set_simple_field(self, field, value):
+        self._parser.set('atomisator', field, value)    
+
+
+    def _set_multiline_value(self, name, value):
+
+        def _quote(value):
+            if ' ' in value:
+                value = value.replace('"', "'")
+                return '"%s"' % value
+            return value
+
+        def _line(entry):
+            main = entry[0]
+            params = [_quote(e) for e in entry[1]]
+            return '%s %s' % (main, ' '.join(params))
+        
+        values = '\n'.join([_line(v) for v in value])
+        self._parser.set('atomisator', name, values)
+
+    def _get_multiline_value(self, name):
+        v = self._parser.get('atomisator', name).split('\n')
+        # crappy pattern matching
+        def _rep(match):
+            return match.groups()[0].replace(' ', ':::')
+        def _args(p):
+            p = re.sub(r'"(.*?)"', _rep, p)
+            p = [e.replace(':::', ' ') 
+                 for e in p.split() if e != '']
+            return p[0].strip(), tuple([p.strip() 
+                                        for p in p[1:]])
+        return [_args(s) for s in v if s.strip() != '']
+
+    # 
+    # properties and public APIs
+    #
     def write(self):
         f = open(self._file, 'w')
         try:
             self._parser.write(f)
         except:
             f.close()
-
-    def _set_multiline_value(self, name, value):
-
-        def _line(entry):
-            return '%s %s' % (entry[0], ' '.join(entry[1]))
-        values = '\n'.join([_line(v) for v in value])
-        self._parser.set('atomisator', name, values)
 
     def get_reader(self, name):
         if not self._parser.has_section('readers'):
@@ -39,82 +77,38 @@ class AtomisatorConfig(object):
         return self._parser.get('readers', name)
 
     def _get_sources(self):
-        sources = self._parser.get('atomisator', 'sources').split('\n')
-        def _args(p):
-            p = p.split()
-            return p[0].strip(), tuple([p.strip() 
-                                        for p in p[1:]])
-        return [_args(s) for s in sources if s.strip() != '']
-
+        return self._get_multiline_value('sources')
     def _set_sources(self, sources):
         self._set_multiline_value('sources', sources)
-
     sources = property(_get_sources, _set_sources)
 
     def _get_outputs(self):
-        outputs = self._parser.get('atomisator', 'outputs').split('\n')
-        def _args(p):
-            p = p.split()
-            return p[0].strip(), tuple([p.strip() 
-                                        for p in p[1:]])
-        return [_args(s) for s in outputs if s.strip() != '']
-
+        return self._get_multiline_value('outputs') 
     def _set_outputs(self, outputs):
         self._set_multiline_value('outputs', outputs)
-
     outputs = property(_get_outputs, _set_outputs)
 
     def _get_filters(self):
-        if not self._parser.has_option('atomisator', 'filters'):
-            return []
-        filters = self._parser.get('atomisator', 'filters').split('\n')
-        def _args(p):
-            p = p.split()
-            return p[0].strip(), tuple([p.strip() 
-                                        for p in p[1:]])
-        return [_args(s) for s in filters if s.strip() != '']
-
+        return self._get_multiline_value('filters') 
     def _set_filters(self, filters):
         self._set_multiline_value('filters', filters)
-
     filters = property(_get_filters, _set_filters)
 
     def _get_enhancers(self):
-        if not self._parser.has_option('atomisator', 'enhancers'):
-            return []
-        enhancers = self._parser.get('atomisator', 'enhancers').split('\n')
-        def _args(p):
-            p = p.split()
-            return p[0].strip(), tuple([p.strip() 
-                                        for p in p[1:]])
-        return [_args(e) for e in enhancers if e.strip() != '']
-    
+        return self._get_multiline_value('enhancers')
     def _set_enhancers(self, value):
         self._set_multiline_value('enhancers', value)
-
     enhancers = property(_get_enhancers, _set_enhancers)
-
-    def _get_simple_field(self, field, default=None):
-        if not self._parser.has_option('atomisator', field):
-            return default
-        return self._parser.get('atomisator', field).strip()
-    
-    def _set_simple_field(self, field, value):
-        self._parser.set('atomisator', field, value)    
 
     def _get_database(self):
         return self._get_simple_field('database')
-
     def _set_database(self, value):
         self._set_simple_field('database', value)
-
     database = property(_get_database, _set_database)
 
     def _get_timeout(self):
         return self._get_simple_field('timeout', '5')
-    
     def _set_timeout(self, value):
         self._set_simple_field('timeout', value)
-
     timeout = property(_get_timeout, _set_timeout)
 
