1623ec3f6ce4d3e997035e958b53f634c99840f4
tarek <tarek@localhost>
2008-11-04 20:37:53 +0100

    finished the mail output plugin

--- packages/atomisator.outputs/atomisator/outputs/mail.py
+++ packages/atomisator.outputs/atomisator/outputs/mail.py
@@ -4,12 +4,69 @@
 Mail output plugin.
 """
 from email.MIMEText import MIMEText
+from smtplib import SMTP 
+from datetime import datetime 
+
+DEFAULT_BODY_TMPL = """\
+Atomisator has triggered an alert. Check out for:
+
+%(entries)s
+"""
+
+DEFAULT_ENTRY_TMPL = """\
+  * %(title)s: %(url)s
+"""
 
 class Mail(object):
-    """Will send an alert by mail"""
+    """Will send an alert by mail
+    
+    Arguments:
+        - emails to send the alert to, separated by commas
+        - subject (optional)
+        - from mail (optional)
+        - path to a template file for the body (optional)
+        - path to a template file for the entry (optional)
+        - smtp server (optional)
+        - smtp port (optional)
+    """
 
-    def __call__(self, entries, enhancers, args):
+    def __call__(self, entries, args):
         
+        # do not send a mail on empty entries
+        if len(entries) == 0:
+            return
+        values = {'tos': args[0].split(',')}
+
+        optionals = (('subject', 'Atomisator alert'),
+                     ('from', 'admin@atomisator'),
+                     ('body_template', DEFAULT_BODY_TMPL),
+                     ('entry_template', DEFAULT_ENTRY_TMPL),
+                     ('smtp_server', 'localhost'),
+                     ('smtp_port', '25')
+                     )
+        
+        for pos, default in enumerate(optionals):
+            pos += 1
+            name, default = default
+            values[name] = len(args) > pos and args[pos] or default
+    
+        # lines
+        lines = [values['entry_template'] % entry for entry in entries]
+        
+        # mail content
+        text = values['body_template'] % \
+                {'entries': '\n'.join(lines)}
+
         # a mail is built, using MimeText
-        msg = MIMEText() 
+        msg = MIMEText(text)
+        msg['Subject'] = values['subject']
+        msg['To'] = ','.join(values['tos'])
+        msg['From'] = values['from']
+        msg['Date'] = datetime.now().isoformat()
+        msg = msg.as_string() 
+
+        # let's send it
+        smtp = SMTP(values['smtp_server'], values['smtp_port'])
+        smtp.sendmail(values['from'], values['tos'], msg)
+
 
---
+++ packages/atomisator.outputs/atomisator/outputs/tests/test_mail.py
@@ -0,0 +1,70 @@
+import smtplib
+from nose.tools import *
+
+class _SMTP(object):
+
+    msgs = []
+
+    def __init__(self, *args, **kw):
+        pass
+
+    def sendmail(self, from_, tos, msg, **kw):
+        self.msgs.append({'from': from_,
+                          'tos': tos, 'msg': msg})
+
+smtplib.SMTP = _SMTP
+
+import datetime
+
+class _datetime(object):
+    @classmethod
+    def now(cls):
+        return cls
+    @classmethod
+    def isoformat(cls):
+        return 'NOW'
+datetime.datetime = _datetime
+
+# required because nose dicovering loads mail...
+# so the patch for SMTP was not applied
+import atomisator.outputs.mail
+reload(atomisator.outputs.mail)
+from atomisator.outputs.mail import Mail 
+
+waited = """\
+Content-Type: text/plain; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Subject: Atomisator alert
+To: tarek@ziade.org
+From: admin@atomisator
+Date: NOW
+
+Atomisator has triggered an alert. Check out for:
+
+  * this: http://here
+
+"""
+
+def test_email():
+
+    email = Mail()
+
+    # args : 
+    #   to mails, subject, from mail, 
+    #   body_template, entry_template
+    #   smtp_server, smtp_port
+    
+    entry = {'url': 'http://here', 'title': 'this'}
+    args = ('tarek@ziade.org',)
+    entries = [entry]
+    email(entries, args)
+
+    assert len(_SMTP.msgs) == 1
+    msg = _SMTP.msgs[0]
+
+    assert msg['from'] == 'admin@atomisator'
+    assert_equals(msg['tos'], ['tarek@ziade.org'])
+
+    assert_equals(msg['msg'], waited)
+
