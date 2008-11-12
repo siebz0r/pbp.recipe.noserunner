8295205a0b366f0c84ebc93c186d3b519f0323e8
tarek <tarek@localhost>
2008-11-12 07:40:40 +0100

    pylinted

--- packages/atomisator.main/atomisator/main/__init__.py
+++ packages/atomisator.main/atomisator/main/__init__.py
@@ -8,12 +8,11 @@ from setuptools.package_index import iter_entry_points
 
 def _load_entry_point(name):
     """Loads an atomisator entry point."""
-    it = iter_entry_points(name)
-    return dict([(e.name, e.load()()) 
-                 for e in it])
+    entry_points = iter_entry_points(name)
+    return dict([(e.name, e.load()()) for e in entry_points])
 
-filters = _load_entry_point('atomisator.filters')
-outputs = _load_entry_point('atomisator.outputs')
-enhancers = _load_entry_point('atomisator.enhancers')
-readers = _load_entry_point('atomisator.readers')
+FILTERS = _load_entry_point('atomisator.filters')
+OUTPUTS = _load_entry_point('atomisator.outputs')
+ENHANCERS = _load_entry_point('atomisator.enhancers')
+READERS = _load_entry_point('atomisator.readers')
 
--- packages/atomisator.main/atomisator/main/core.py
+++ packages/atomisator.main/atomisator/main/core.py
@@ -13,10 +13,10 @@ from multiprocessing import TimeoutError
 from atomisator.main.config import log
 from atomisator.main.config import dotlog
 from atomisator.main.config import AtomisatorConfig
-from atomisator.main import filters
-from atomisator.main import outputs
-from atomisator.main import enhancers
-from atomisator.main import readers
+from atomisator.main import FILTERS
+from atomisator.main import OUTPUTS
+from atomisator.main import ENHANCERS
+from atomisator.main import READERS
 from atomisator.db.session import create_session
 from atomisator.db.core import create_entry
 from atomisator.db.core import get_entries
@@ -37,16 +37,16 @@ def _select_enhancers(enhancers_selected):
     """Gets the selected enhancers."""
     res = []
     for name, args in enhancers_selected:
-        if name in enhancers:
-            res.append((enhancers[name], args))
+        if name in ENHANCERS:
+            res.append((ENHANCERS[name], args))
     return res
 
 def _select_outputs(outputs_selected):
     """Gets the selected outputs."""
     res = []
     for name, args in outputs_selected:
-        if name in outputs:
-            res.append((outputs[name], args))
+        if name in OUTPUTS:
+            res.append((OUTPUTS[name], args))
     return res
 
 def _apply_filters(entry, entries, selected_filters):
@@ -108,17 +108,16 @@ class DataProcessor(object):
         self.existing_entries = get_entries().all()
 
         # building filtering chain once.
-        log('Loading filters plugins : %s' \
-                   % ', '.join([f for f, a in self.parser.filters])) 
-        self.filter_chain = set([(_load_plugin(name, filters), args) 
+        filter_names = [f[0] for f in self.parser.filters]
+        log('Loading filters plugins : %s' % ', '.join(filter_names))
+        self.filter_chain = set([(_load_plugin(name, FILTERS), args) 
                                  for name, args in self.parser.filters 
-                                 if name in filters])
+                                 if name in FILTERS])
         
         # building source chain once.
-        log('Loading source plugins : %s' \
-                   % ', '.join([f for f, a in self.parser.sources]))
-
-        sources = [(reader_name, _load_plugin(reader_name, readers), args) 
+        source_names = [s[0] for s in self.parser.sources]
+        log('Loading source plugins : %s' % ', '.join(source_names))
+        sources = [(reader_name, _load_plugin(reader_name, READERS), args) 
                    for reader_name, args in self.parser.sources]
 
         # creating a processing pool
@@ -146,9 +145,9 @@ class DataProcessor(object):
                                    self.filter_chain)
             if entry is None:
                 continue
-            id_, new_entry = create_entry(entry, commit=False,
-                                          session=psession)
-            self.existing_entries.append(new_entry)
+            new_entry = create_entry(entry, commit=False,
+                                     session=psession)
+            self.existing_entries.append(new_entry[1])
         psession.commit()
 
     def generate_data(self):
@@ -164,19 +163,20 @@ class DataProcessor(object):
             # Enhancement is a two-phase process.
             # 1. Preparing entries for enhancement
             log('Preparing enhancers.')
-            for e, args in selected_enhancers:
-                if hasattr(e, 'prepare'):
+            for enhancer, args in selected_enhancers:
+                if hasattr(enhancer, 'prepare'):
                     dotlog('.')
-                    e.prepare(entries)
+                    enhancer.prepare(entries)
             dotlog('\n')
 
             # 2. Now enhancing them
             log('Enhancing: %s' % ', '.join([e for e, args 
                                             in self.parser.enhancers]))
             def _enhance(entry):
-                for e, args in selected_enhancers:
+                """Enhances an entry"""
+                for enhancer, args in selected_enhancers:
                     dotlog('.')
-                    entry = e(entry, *args)
+                    entry = enhancer(entry, *args)
                 return entry
         
             entries = [_enhance(e) for e in entries]
@@ -184,7 +184,7 @@ class DataProcessor(object):
 
         if selected_outputs != []:
             log('Writing outputs.')
-            log('Rendering: %s' % ', '.join([e for e, args 
+            log('Rendering: %s' % ', '.join([output[0] for output 
                                              in self.parser.outputs]))
             for output, args in selected_outputs:
                 dotlog('.')
