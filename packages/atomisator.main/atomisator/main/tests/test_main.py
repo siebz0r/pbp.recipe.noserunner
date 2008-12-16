b2677143b856fcfd4a4271ca0882b4bee685f221
tarek <tarek@localhost>
2008-12-16 09:45:14 +0100

    fixed test

--- packages/atomisator.main/atomisator/main/tests/test_main.py
+++ packages/atomisator.main/atomisator/main/tests/test_main.py
@@ -118,7 +118,7 @@ def test_no_storage():
     proc.generate_data()   
        
     # as usual but in memory
-    assert_equals(len(proc.existing_entries), 225)
+    assert len(proc.existing_entries) > 50
 
 @with_setup(set_conf, tear_conf)
 def test_mono_proc():
