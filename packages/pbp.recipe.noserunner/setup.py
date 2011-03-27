3080910d0ea4bb403638dffe8c23f5c3b381c727
Aurelien Bompard <aurelien@bompard.org>
2011-03-27 12:55:59 +0200

    Update metadata

--- packages/pbp.recipe.noserunner/setup.py
+++ packages/pbp.recipe.noserunner/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.2.6'
+version = '0.2.7'
 
 long_description = (
     read('README.txt')
@@ -57,7 +57,7 @@ setup(name='pbp.recipe.noserunner',
       include_package_data=True,
       zip_safe=False,
       install_requires=['setuptools',
-                        'zc.buildout',
+                        'zc.buildout >= 1.5.0',
                         'nose',
                         'zc.recipe.egg'
                         # -*- Extra requirements: -*-
